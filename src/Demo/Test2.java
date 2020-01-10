a238782c72b48e456813e581a1cebd30e3f9695f
Satish <satishvasekar147@gmail.com>
2020-01-10 10:07:21 +0530

    adding new change to the code in test2 file

--- src/Demo/Test2.java
+++ src/Demo/Test2.java
@@ -2,4 +2,8 @@ package Demo;
 
 public class Test2 {
 
+	public static void main(String[] args) {
+		System.out.println("this is the new file");
+		System.out.println("Adding changes to the file");
+	}
 }
